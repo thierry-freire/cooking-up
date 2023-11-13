c56b4430b3914db8ea944d26510b6f87012f77bd
thierry.santos <thierry.santos@zitrus.com.br>
2023-11-13 16:40:34 -0300

    Adição dos componentes de Tag.

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,5 +1,7 @@
 <script lang="ts">
     import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import SuaLista from './SuaLista.vue';
+    import Tag from './Tag.vue';
 
     export default {
     data() {
@@ -7,30 +9,15 @@
             ingredientes: []
         };
     },
-    components: { SelecionarIngredientes }
+    components: { SelecionarIngredientes, Tag, SuaLista }
 }
 </script>
 
 <template>
     <main class="conteudo-principal">
-    <section>
-      <span class="subtitulo-lg sua-lista-texto">
-        Sua lista:
-      </span>
-
-      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
-            {{ ingrediente }}
-        </li>
-      </ul>
-
-      <p v-else class="paragrafo lista-vazia">
-        <img src="@/assets/imagens/icones/lista-vazia.svg" alt="Ícone de pesquisa">
-      </p>
-    </section>
-
-    <SelecionarIngredientes />
-  </main>
+        <SuaLista :ingredientes="ingredientes" />
+        <SelecionarIngredientes />
+    </main>
 </template>
 
 <style scoped>
@@ -44,43 +31,6 @@
         flex-direction: column;
         align-items: center;
         gap: 5rem;
-    }
-
-    .sua-lista-texto {
-        color: var(--coral, #F0633C);
-        display: block;
-        text-align: center;
-        margin-bottom: 1.5rem;
-    }
-
-    .ingredientes-sua-lista {
-        display: flex;
-        justify-content: center;
-        gap: 1rem 1.5rem;
-        flex-wrap: wrap;
-    }
-
-    .ingrediente {
-        display: inline-block;
-        border-radius: 0.5rem;
-        min-width: 4.25rem;
-        padding: 0.5rem;
-        text-align: center;
-            transition: 0.2s;
-            color: var(--creme, #FFFAF3);
-        background: var(--coral, #F0633C);
-        font-weight: 700;   
-    }
-
-    .lista-vazia {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-wrap: wrap;
-        gap: 0.25rem;
-
-        color: var(--coral, #F0633C);
-        text-align: center;
     }
 
     @media only screen and (max-width: 1300px) {
